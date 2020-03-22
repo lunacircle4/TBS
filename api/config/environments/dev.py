bb1149c773bcef02aaed5ce1ff94ad2ca1e9aa56
DONGWON KIM <lunacircle4@likelion.org>
2020-03-23 00:00:10 +0900

    feat: 환경 분리

---
+++ api/config/environments/dev.py
@@ -0,0 +1,24 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'db',
+        'NAME': 'tbs_development',
+        'USER': 'root',
+        'PASSWORD': 'tbs_development_1234',
+        'PORT': '3306',
+    }   
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
