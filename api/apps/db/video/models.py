0d546f7a2c57ffcc9174174b2d23961e061dbc22
DONGWON KIM <lunacircle4@likelion.org>
2020-03-22 23:59:00 +0900

    feat: Video 모델 video_url 필드 이름 변경

--- api/apps/db/video/models.py
+++ api/apps/db/video/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from apps.db.interface import Timestamp
 
 class Video(Timestamp):
-    video_url = models.FileField(upload_to="user_videos/", max_length=100)
+    video = models.FileField(upload_to="user_videos/", max_length=100)
     class Meta:
         db_table = "video"
